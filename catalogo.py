c17262b002d9ed3186c7fb58f46f00dbc3b507a4
gifuentes92 <isa2_7_92@hotmail.com>
2024-06-15 20:28:18 -0300

    agregado atributo privado a clase película y métodos para que se pueda modificar

--- catalogo.py
+++ catalogo.py
@@ -1,39 +1,45 @@
-import os
+
+
+import os 
 
 class Pelicula:
     def __init__(self, titulo, director, anio):
-        self.titulo = titulo
+        self.__titulo = titulo  #atributo privado
         self.director = director
         self.anio = anio
 
+#metodos property y setter para poder modificar el atributo privado "titulo"
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, nuevo_titulo):
+        self.__titulo = nuevo_titulo
+
     def __str__(self):
-        return f"Película: {self.titulo}, {self.director}, {self.anio}"
+        return f"Película: {self.__titulo}, {self.director}, {self.anio}"
+
 
 class catalogoPeliculas:
-    ruta_archivo = "catalogo.txt"
 
+    ruta_archivo = "catalogo.txt"
     @classmethod
-    def agregar_pelicula(cls, pelicula):
-        with open(cls.ruta_archivo, "a", encoding='utf8') as archivo:
-            archivo.write(f"{pelicula.titulo},{pelicula.director},{pelicula.anio}\n")
+    def agregar_pelicula(cls,pelicula):
+     with open(cls.ruta_archivo,"a",encoding= 'utf8') as archivo:
+        archivo.write(f"{pelicula.titulo},{pelicula.director},{pelicula.anio}\n")
 
     @classmethod
     def listar_peliculas(cls):
-        try:
-            with open(cls.ruta_archivo, "r", encoding='utf8') as archivo:
-                for linea in archivo:
-                    titulo, director, anio = linea.strip().split(",")
-                    print(f"Película: {titulo}, {director}, {anio}")
-        except FileNotFoundError:
-            print("El catálogo está vacío.")
+        with open(cls.ruta_archivo,"r",encoding= 'utf8') as archivo:
+            for linea in archivo:
+                titulo, director, anio = linea.split(",")
+                print(f"Película: {titulo}, {director}, {anio}")
 
     @classmethod
     def eliminar_catalogo(cls):
-        if os.path.exists(cls.ruta_archivo):
-            os.remove(cls.ruta_archivo)
-            print(f"Catálogo {cls.ruta_archivo} eliminado con éxito.")
-        else:
-            print("El catálogo no existe.")
+        os.remove(cls.ruta_archivo)
+        print("Catálogo eliminado con éxito")
 
 def mostrar_menu():
     print("~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°~°")
@@ -71,5 +77,6 @@ def main():
             print("Por favor, ingrese un número válido.")
         except Exception as e:
             print(f"Ha ocurrido un error!: {e}")
+
 
 main()
